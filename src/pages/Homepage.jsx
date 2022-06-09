0d7e2756fa067273be95d95c9aed928c618e7862
LorenaCoheneBaez <loreley.cb15@gmail.com>
2022-06-09 18:59:10 -0300

    Corrijo margenes de homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,12 +3,14 @@ import { apiEntity } from "../services/apiConfig";
 import Banner from "../components/Banner/Banner";
 import Carousel from "../components/Carousel/Carousel";
 import Separator from "../components/Separator/Separator";
+import styles from "./Homepage.module.css"
 
 const Homepage = () => {
   return (
     <>
       <Banner />
       <Separator height={"40px"} />
+      <div className={styles.carousel_container}>
       <Carousel
         entity={apiEntity.popularMovies}
         title={"Películas populares"}
@@ -22,6 +24,7 @@ const Homepage = () => {
       <Carousel entity={apiEntity.popularTv} title="Series populares" />
       <Separator height={"40px"} />
       <Carousel entity={apiEntity.topRatedTv} title="Series mejor puntuadas" />
+      </div>
     </>
   );
 };
